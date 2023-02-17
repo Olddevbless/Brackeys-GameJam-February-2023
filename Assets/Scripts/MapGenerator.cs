1ab716dd680dcb095cbbaf931eb1eae51a4e3f84
Olddevbless <Olddevbless@users.noreply.github.com>
2023-02-17 09:55:32 +0200

    importing models

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -25,18 +25,40 @@ public class MapGenerator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                vertices[i] = new Vector3(x, 0, z);
+                float y = Mathf.PerlinNoise(x * 0.3f, z * 0.3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
                 i++;
             }
         }
-        triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = xSize + 1;
-        triangles[2] = 1;
+        triangles = new int[xSize * zSize * 6];
+
+        int verts = 0;
+        int tris = 0;
+        for (int y = 0; y < zSize; y++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[tris + 0] = verts + 0;
+                triangles[tris + 1] = verts + xSize + 1;
+                triangles[tris + 2] = verts + 1;
+                triangles[tris + 3] = verts + 1;
+                triangles[tris + 4] = verts + xSize + 1;
+                triangles[tris + 5] = verts + xSize + 2;
+                verts++;
+                tris += 6;
+            }
+            verts++;
+        }
+
+
     }
     void UpdateShape()
     {
-
+        mesh.Clear();
+        mesh.triangles = triangles;
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
     }
 
     private void OnDrawGizmos()
